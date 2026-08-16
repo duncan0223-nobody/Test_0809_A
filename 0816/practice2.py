72669089bf7066bd4dbec1987f83671c816b4100
duncan0223 <duncan0223@gmail.com>
2026-08-16 13:42:15 +0800

    upload practice3.ipynb via juypter lab

--- 0816/practice2.py
+++ 0816/practice2.py
@@ -7,7 +7,7 @@ client = genai.Client()
 
 interaction = client.interactions.create(
     model="gemini-3.5-flash",
-    input="天空為什麼是藍的"
+    input="為什麼台灣8月天非常熱?"
 )
 
 print(interaction.output_text)
